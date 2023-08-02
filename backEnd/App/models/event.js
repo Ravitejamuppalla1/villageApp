f1556b85d039600b0ed4334fc468f21780b8309a
Raviteja <teja.muppalla9397@gmail.com>
2023-08-02 20:19:44 +0530

    code changes

--- backEnd/App/models/event.js
+++ backEnd/App/models/event.js
@@ -7,11 +7,11 @@ const eventSchema=new Schema({
         required:true
     },
     startDate:{
-        type:Date,
+        type:String,
         required:true
     },
     endDate:{
-        type:Date,
+        type:String,
         required:true
     },
     description:{
